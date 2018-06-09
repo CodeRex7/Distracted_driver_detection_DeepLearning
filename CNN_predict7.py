5047010c1f5f9af78185185182c83589647d56ea
SoumyaGourab <gourab.soumya@outlook.com>
2018-06-09 15:28:00 +0530

    Add files via upload

---
+++ CNN_predict7.py
@@ -0,0 +1,108 @@
+from keras.models import load_model
+import cv2
+import numpy as np
+import tkinter as tk
+from PIL import ImageTk, Image
+from tkinter import filedialog
+from tkinter import StringVar
+from tkinter import Entry
+
+#This creates the main window of an application
+window = tk.Tk()
+window.title("Join")
+window.geometry("900x900")
+window.configure(background='grey')
+
+frame = tk.Frame(window, bg='black')
+frame.pack(fill='both', expand='yes')
+
+path = filedialog.askopenfilename()
+img = ImageTk.PhotoImage(Image.open(path))
+
+#The Label widget is a standard Tkinter widget used to display a text or image on the screen.
+panel = tk.Label(frame, image = img)
+panel.place(x=100,y=20)
+#The Pack geometry manager packs widgets in rows or columns.
+#panel.pack(side = "bottom", fill = "both", expand = "yes")
+#mainFrame = tk.Frame(window)
+#mainFrame.grid()
+#entryFrame = tk.Frame(mainFrame, width=454, height=20)
+#entryFrame.grid(row=0, column=1)
+
+
+
+#v = StringVar()
+#e = Entry(frame, textvariable=v, width=200)
+#e.place(x=20,y=700)
+#e.pack(ipady=3)
+
+model = load_model('model3.h5')
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+# Set the path of the image to be classified
+img1 = cv2.imread(path)
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+while True:
+    cv2.namedWindow("show",0)
+    new_img = img1.copy()
+    new_img = cv2.resize(new_img,(150,150))
+    new_img = np.reshape(new_img,[1,150,150,3])
+    cv2.putText(img1,'OpenCV',(5,160), font, 0.5,(255,0,0),1,cv2.LINE_AA)
+    cla = model.predict_classes(new_img)
+    #v.set("a default value")
+    print (cla[0])
+    if(cla[0] == 1):
+        label = tk.Label(frame, text="Alert! You're turning towards the right & texting instead of concentrating on driving straight.  ")
+        label.place(x=100, y=600)
+        #v.set ("This image can be described as a bar chart which represents data with rectangular bars with heights proportional to the values that they represent.")
+    elif(cla[0] == 2):
+        label = tk.Label(frame, text="Alert! You're turning towards the right & talking over phone instead of concentrating on driving straight.")
+        label.place(x=100, y=600)
+        #v.set ("This image is a graph of a function. It has x and y axes with curves and lines depicting a function.")
+    elif(cla[0] == 3):
+        label = tk.Label(frame, text="Alert! You're turning towards the left & texting instead of concentrating on driving straight.")
+        label.place(x=100, y=600)
+        #v.set ("This image has a geometrical figure.")
+    elif(cla[0] == 4):
+        label = tk.Label(frame, text="Alert! You're turning towards the left & talking over phone instead of concentrating on driving straight.")
+        label.place(x=100, y=600)
+        #v.set ("This image is a line graph in which information is displayed as a series of data points connected by straight line segments.")
+    elif(cla[0] == 5):
+        label = tk.Label(frame, text="Alert! You're operating on the radio instead of concentrating on driving straight. ")
+        label.place(x=100,y=600)
+        #v.set ("This image is a mapping of function which shows the relations of inputs and output in form of ordered pair. ")
+    elif(cla[0] == 6):
+        label = tk.Label(frame, text="Alert! You're drinking instead of concentrating on driving straight.")
+        label.place(x=100, y=600)
+        #v.set ("This image doesnot belong to any mathematical representation or figures.")                                                                                   
+    elif(cla[0] == 7):
+        label = tk.Label(frame, text="Alert! You're reaching behind instead of concentrating on driving straight.")
+        label.place(x=100, y=600)
+        #v.set ("This image is a pie chart, a circle which is divided into sectors that each represent a proportion of the whole.")
+    elif(cla[0] == 8):
+        label = tk.Label(frame, text="Alert! You're preoccupied with the mirror instead of concentrating on driving straight.")
+        label.place(x=100, y=600)
+        #v.set ("This image is a graph with x and y axes where points are plotted and drawn.")
+    elif(cla[0] == 9):
+        label = tk.Label(frame, text="Alert! You're reaching behind instead of concentrating on driving straight.")
+        label.place(x=100, y=600)
+    elif(cla[0] == 0):
+        label = tk.Label(frame, text="You're driving attentively.")
+        label.place(x=350, y=600)
+        #v.set ("This image is a venn diagram in which logical sets represented as circles or closed curves within an enclosing rectangle,the universal set.")
+    cv2.imshow("show",img1)
+
+    #Start the GUI
+    window.mainloop()
+    if cv2.waitKey(0) & 0xFF == ord('q'):
+        break
+
+def write(string):
+    text_box.config(state=Tkinter.NORMAL)
+    text_box.insert("end", string + "\n")
+    text_box.see("end")
+    text_box.config(state=Tkinter.DISABLED)
--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# Distracted_driver_detection_DeepLearning
-Alarming a Driver based on detecting the behaviour of the driver using deep Learning Techniques on live video footage from the dashboard of the car to minimize accidents on the road and can be a first step towards a project for improving the self driving car concept.
+# Distracted-Driver-Detection-with-Deep-Learning
+This project aims to detect the dangerous status of driving based on the images captured by the dashboard camera using deep learning.
+
+# Dataset
+
+The dataset is obtained from 
+
+https://www.kaggle.com/c/state-farm-distracted-driver-detection/data
+
+The dataset contains 22,424 images which belongs to one of the 10 classes given below:
+
+    c0: safe driving
+    
+    c1: texting - right
+    
+    c2: talking on the phone - right
+    
+    c3: texting - left
+    
+    c4: talking on the phone - left
+    
+    c5: operating the radio
+    
+    c6: drinking
+    
+    c7: reaching behind
+    
+    c8: hair and makeup
+    
+    c9: talking to passenger
+    
+We split the data into two sets: training set containing 20,924 images, and validation set containing 1500 images (e.g., 150 images for each class).
+
+# Method 1: train a small Convolutional Neural Network (CNN) from the scatch
+
+Method 1 is implemented in "distracted_driver_detection_with_small_CNN.py". The small CNN consists of 3 convolutional layers with filter size of 3x3, each of which is followed by a max-pooling layer with pool isize of 2x2, and 2 fully-connected dense layers.
+
+
+# Method 2: train a two-layer dense neural network on top of a pre-trained VGG16 deep CNN
+
+VGG16 is a 16-layer CNN used by the VGG team in the ILSVRC-2014 competition. The VGG16 network structure can be seen in "vgg16_CNN.png". The weights of pre-trained VGG16 CNN can be found at:
+
+https://gist.github.com/baraldilorenzo/07d7802847aaad0a35d3
+
+Method 2 is implemented in "distracted_driver_detection_with_pretrained_VGG16_deep_CNN.py". The model consists of two parts: the lower part is a pre-trained VGG network with fozen weights, and the upper part is a two-layer dense network. The model is trained using the dataset.  
+
+
+
+
+
+
+
